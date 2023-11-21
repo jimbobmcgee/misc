bb2f91cf3c726a24b46c66d5a97e051db0b75b46
jimbobmcgee <jimbobmcgee@users.noreply.github.com>
2023-11-21 21:59:00 +0000

    Update ubo.js

--- ubo.js
+++ ubo.js
@@ -2,15 +2,40 @@
 
 /// noscript-to-span.js
 function noscriptToSpan() {
-    console.log('ubo.js: noscript-to-span');
-    var noscripts = document.querySelectorAll
-                  ? document.querySelectorAll('noscript')
-                  : document.getElementsByTagName('noscript');
-    for (var i = 0, ilen = noscripts.length; i < ilen; i++) {
-        var s = document.createElement('span');
-        s.classList.add('ubo-was-noscript');
-        s.innerHTML = noscripts[i].innerText;
-        noscripts[i].parentNode.replaceChild(s, noscripts[i]);
+    console.log('ubo.js: noscript-to-span: starting');
+    
+    const fn = func() {
+        console.log('ubo.js: noscript-to-span: running');
+        
+        try {
+            var noscripts = document.querySelectorAll
+                          ? document.querySelectorAll('noscript')
+                          : document.getElementsByTagName('noscript');
+            for (var i = 0, ilen = noscripts.length; i < ilen; i++) {
+                var s = document.createElement('span');
+                s.classList.add('ubo-was-noscript');
+                s.innerHTML = noscripts[i].innerText;
+                noscripts[i].parentNode.replaceChild(s, noscripts[i]);
+            }
+        }
+        catch (e) {
+            console.log('ubo.js: noscript-to-span: error = ' + e);
+        }
+    };
+
+    const observer = new MutationObserver(fn);
+    
+	observer.observe(document.documentElement, { 
+        attributes: true, 
+        childList: true, 
+        subtree: true
+    });
+    
+	if (document.readyState === "complete") { 
+        self.setTimeout(func() {
+            console.log('ubo.js: noscript-to-span: disconnecting');
+            observer.disconnect();
+        }, 67);  
     }
 }
 
