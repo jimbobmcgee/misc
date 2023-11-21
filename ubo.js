48c1ad0cb71efc35217fa355edf72bca6d2f1e04
jimbobmcgee <jimbobmcgee@users.noreply.github.com>
2023-11-21 22:02:33 +0000

    Update ubo.js

--- ubo.js
+++ ubo.js
@@ -1,41 +1,34 @@
-'use strict';
-
 /// noscript-to-span.js
 function noscriptToSpan() {
-    console.log('ubo.js: noscript-to-span: starting');
-    
-    const fn = func() {
-        console.log('ubo.js: noscript-to-span: running');
-        
-        try {
-            var noscripts = document.querySelectorAll
-                          ? document.querySelectorAll('noscript')
-                          : document.getElementsByTagName('noscript');
-            for (var i = 0, ilen = noscripts.length; i < ilen; i++) {
-                var s = document.createElement('span');
-                s.classList.add('ubo-was-noscript');
-                s.innerHTML = noscripts[i].innerText;
-                noscripts[i].parentNode.replaceChild(s, noscripts[i]);
-            }
-        }
-        catch (e) {
-            console.log('ubo.js: noscript-to-span: error = ' + e);
-        }
-    };
-
-    const observer = new MutationObserver(fn);
-    
-	observer.observe(document.documentElement, { 
-        attributes: true, 
-        childList: true, 
-        subtree: true
-    });
-    
-	if (document.readyState === "complete") { 
-        self.setTimeout(func() {
-            console.log('ubo.js: noscript-to-span: disconnecting');
-            observer.disconnect();
-        }, 67);  
+  console.log('ubo.js: noscript-to-span: starting');  
+  const fn = func() {
+    console.log('ubo.js: noscript-to-span: running');
+    try {
+      var noscripts = document.querySelectorAll
+                    ? document.querySelectorAll('noscript')
+                    : document.getElementsByTagName('noscript');
+      for (var i = 0, ilen = noscripts.length; i < ilen; i++) {
+        var s = document.createElement('span');
+        s.classList.add('ubo-was-noscript');
+        s.innerHTML = noscripts[i].innerText;
+        noscripts[i].parentNode.replaceChild(s, noscripts[i]);
+      }
     }
+    catch (e) {
+      console.log('ubo.js: noscript-to-span: error = ' + e);
+    }
+  };
+  const observer = new MutationObserver(fn);
+  observer.observe(document.documentElement, { 
+    attributes: true, 
+    childList: true, 
+    subtree: true
+  });
+    
+  if (document.readyState === "complete") { 
+    self.setTimeout(func() {
+      console.log('ubo.js: noscript-to-span: disconnecting');
+      observer.disconnect();
+    }, 67);  
+  }
 }
-
