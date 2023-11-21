fb2898eece1f38065d2023467879d85a81276d2c
jimbobmcgee <jimbobmcgee@users.noreply.github.com>
2023-11-21 23:01:42 +0000

    Update ubo.js

--- ubo.js
+++ ubo.js
@@ -4,9 +4,7 @@ function noscriptToSpan() {
   const fn = function() {
     console.log('ubo.js: noscript-to-span: running');
     try {
-      var noscripts = document.querySelectorAll
-                    ? document.querySelectorAll('noscript')
-                    : document.getElementsByTagName('noscript');
+      var noscripts = document.getElementsByTagName('noscript');
       for (var i = 0, ilen = noscripts.length; i < ilen; i++) {
         var s = document.createElement('span');
         s.classList.add('ubo-was-noscript');
