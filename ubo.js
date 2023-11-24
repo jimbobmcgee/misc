a2de606b53bffa8dc866b05487c7cf9aff40498b
jimbobmcgee <jimbobmcgee@users.noreply.github.com>
2023-11-24 02:28:49 +0000

    Update ubo.js

--- ubo.js
+++ ubo.js
@@ -1,19 +1,19 @@
-/// noscript-to-span.js
-function noscriptToSpan() {
-  console.log('ubo.js: noscript-to-span: init');
+/// reinject-noscript.js
+function reinjectNoscriptElementContent() {
+  console.log('ubo.js: reinject-noscript: init');
   const fn = function() {
-    console.log('ubo.js: noscript-to-span: running');
+    console.log('ubo.js: reinject-noscript: running');
     try {
       var noscripts = document.getElementsByTagName('noscript');
       for (var i = 0, ilen = noscripts.length; i < ilen; i++) {
-        var s = document.createElement('span');
+        var s = document.createDocumentFragment();
         s.classList.add('ubo-was-noscript');
         s.innerHTML = noscripts[i].innerText;
         noscripts[i].parentNode.replaceChild(s, noscripts[i]);
       }
     }
     catch (e) {
-      console.log('ubo.js: noscript-to-span: error = ' + e);
+      console.log('ubo.js: reinject-noscript: error = ' + e);
     }
   };
   if (document.readyState !== "complete") {
