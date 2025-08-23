bc2bafbe94ed5765a6690bb00cd203f2dca702f5
jimbobmcgee <jimbobmcgee@users.noreply.github.com>
2025-08-23 06:04:05 +0100

    Update ubo.js

--- ubo.js
+++ ubo.js
@@ -7,14 +7,6 @@
       var noscripts = document.getElementsByTagName('noscript') || [];
       for (var i = noscripts.length - 1; i >= 0; i--) {
         if (noscripts[i] !== undefined && noscripts[i] !== null) {
-          var refreshes = [];
-          var metas = noscripts[i].querySelectorAll("meta[http-equiv=refresh]");
-          for (var j = metas.length - 1; j >= 0; j--) {
-            console.log('ubo.js: reinject-noscript: suppressing refresh: ' + metas[j].content);
-            refreshes.unshift(metas[j].content);
-            metas[j].remove();
-          }
-
           var tpl = document.createElement('template');
           tpl.innerHTML = "<!-- ubo.js: reinject-noscript: start -->\n"
                         + noscripts[i].innerText
