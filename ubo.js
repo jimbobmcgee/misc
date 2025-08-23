16936c4e8b28bee1cafcc63a4feac5f72d252f71
jimbobmcgee <jimbobmcgee@users.noreply.github.com>
2025-08-23 05:37:00 +0100

    bad debugging

--- ubo.js
+++ ubo.js
@@ -2,36 +2,7 @@
 (function reinjectNoscriptElementContent() {
   console.log('ubo.js: reinject-noscript: init (v=3)');
   const fn = function() {
-    console.log('ubo.js: reinject-noscript: running');
-    try {
-      var noscripts = document.getElementsByTagName('noscript') || [];
-      for (var i = noscripts.length - 1; i >= 0; i--) {
-        if (noscripts[i] !== undefined && noscripts[i] !== null) {
-          var refreshes = [];
-          var metas = noscripts[i].querySelectorAll("meta[http-equiv=refresh]");
-          for (var j = metas.length - 1; j >= 0; j--) {
-            console.log('ubo.js: reinject-noscript: suppressing refresh: ' + metas[j].content);
-            refreshes.unshift(metas[j].content);
-            metas[j].remove();
-          }
-
-          var tpl = document.createElement('template');
-          tpl.innerHTML = "<!-- ubo.js: reinject-noscript: start -->\n"
-                        + refreshes.map(r => "<!-- ubo.js: suppress-refresh: " + r.replace('--', '%2D%2D') + " -->\n").join("")
-                        + noscripts[i].innerText
-                        + "\n<!-- ubo.js: reinject-noscript: end -->";
-          
-          noscripts[i].parentNode.replaceChild(tpl.content, noscripts[i]);
-        }
-        else {
-          console.log('ubo.js: reinject-noscript: weird = noscripts[' + i + 
-                      '] not defined');
-        }
-      }
-    }
-    catch (e) {
-      console.log('ubo.js: reinject-noscript: error = ' + e);
-    }
+    window.alert('ubo.js: wtf is going on here?');
   };
   if (document.readyState !== "complete") {
     document.addEventListener("DOMContentLoaded", fn);
