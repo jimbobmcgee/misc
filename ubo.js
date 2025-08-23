912eb94477b59a7aa62cfa30642dbf587be52bf3
jimbobmcgee <jimbobmcgee@users.noreply.github.com>
2025-08-23 06:34:54 +0100

    Update ubo.js

--- ubo.js
+++ ubo.js
@@ -3,10 +3,6 @@
   window.alert('ubo.js outer');
   console.log('ubo.js: reinject-noscript: init (v=3.2)');
 
-  var fn = function() {
-    console.log('ubo.js: reinject-noscript: running');
-    window.alert('ubo.js inner');
-  };
   
   window.alert('ubo.js outer 2');
 })();
