46f9f22653b8c58e059dc20c36988a040cc77ee1
jimbobmcgee <jimbobmcgee@users.noreply.github.com>
2025-08-23 05:22:15 +0100

    include version info in initial console log

--- ubo.js
+++ ubo.js
@@ -1,6 +1,6 @@
 /// reinject-noscript.js
 (function reinjectNoscriptElementContent() {
-  console.log('ubo.js: reinject-noscript: init');
+  console.log('ubo.js: reinject-noscript: init (v=3)');
   const fn = function() {
     console.log('ubo.js: reinject-noscript: running');
     try {
