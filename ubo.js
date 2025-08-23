10ea3a7eb169f78df24fab7b44c21531bf8be8ef
jimbobmcgee <jimbobmcgee@users.noreply.github.com>
2025-08-23 06:55:14 +0100

    Update ubo.js

--- ubo.js
+++ ubo.js
@@ -1,6 +1,6 @@
 /// reinject-noscript.js
 (function reinjectNoscriptElementContent() {
-  console.log('ubo.js: reinject-noscript: init (v=3)');
+  console.log('ubo.js: reinject-noscript: init');
   const fn = function() {
     console.log('ubo.js: reinject-noscript: running');
     try {
@@ -16,12 +16,20 @@
           }
           var tpl = document.createElement('template');
           tpl.innerHTML = "<!-- ubo.js: reinject-noscript: start -->\n"
-                        + refreshes.map(r => "<!-- ubo.js: suppress-refresh: " + r.replace('--', '%2D%2D') + " -->\n").join("")
+                        + refreshes.map(function(r) { 
+                            return  "<!-- ubo.js: suppress-refresh: " 
+                                  + r.replace('--', '%2D%2D') 
+                                  + " -->\n";
+                          }).join("")
                         + noscripts[i].innerText
-                        + "\n<!-- ubo.js: reinject-noscript: end -->";          
+                        + "\n<!-- ubo.js: reinject-noscript: end -->";
+          
           noscripts[i].parentNode.replaceChild(tpl.content, noscripts[i]);
         }
-        else console.log('ubo.js: reinject-noscript: weird = noscripts[' + i + '] not defined');
+        else {
+          console.log('ubo.js: reinject-noscript: weird = noscripts[' + i + 
+                      '] not defined');
+        }
       }
     }
     catch (e) {
