8a3cea634157e008fee38b62eb86e6473947a057
jimbobmcgee <jimbobmcgee@users.noreply.github.com>
2025-08-23 06:19:32 +0100

    Update ubo.js

--- ubo.js
+++ ubo.js
@@ -1,9 +1,15 @@
 /// reinject-noscript.js
 (function reinjectNoscriptElementContent() {
-  console.log('ubo.js: reinject-noscript: init (v=3)');
+  console.log('ubo.js: reinject-noscript: init (v=3.2)');
+  
   const fn = function() {
-    window.alert('ubo.js: wtf is going on here?');
+    console.log('ubo.js: reinject-noscript: running');
+    var noscripts = document.getElementsByTagName('noscript') || [];
+    for (var i = noscripts.length - 1; i >= 0; i--) {
+      console.log('ubo.js: reinject-noscript: noscript #' + i);
+    }
   };
+  
   if (document.readyState !== "complete") {
     document.addEventListener("DOMContentLoaded", fn);
   }
